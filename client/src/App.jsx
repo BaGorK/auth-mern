ef226c5d1e516aaef0f2953115a91a919d6ea49c
BaGorK <edmealemkassahun@gmail.com>
2024-05-05 22:21:45 +0300

    feat: create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,5 @@
 export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <h1 className='text-3xl font-bold underline text-red-400'>Hello world!</h1>
+  );
 }
