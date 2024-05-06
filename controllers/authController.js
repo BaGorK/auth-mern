3dbb86ee996c8c7cdcf2a435ee312d9c4f3ee6a1
BaGorK <edmealemkassahun@gmail.com>
2024-05-06 11:11:15 +0300

    feat: create auth controller

---
+++ controllers/authController.js
@@ -0,0 +1,3 @@
+export const signup = (req, res) => {
+  return res.status(200).json({ message: 'signup' });
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import { signup } from '../controllers/authController.js';
 
 const Router = express.Router();
 
-Router.post('/signup', (req, res) => res.send('signup'));
+Router.post('/signup', signup);
 
 export default Router;
