c57401d480d42f9ebc3e94dcc39730230fc6b683
BaGorK <edmealemkassahun@gmail.com>
2024-05-06 09:40:58 +0300

    feat: set up a simple server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('hello from the server');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  return res.send('hello from the server!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`server listening on port ${PORT}...`));
