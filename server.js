1a9f1bff2d1bff60a02ec97a5f45c93f3f717563
BaGorK <edmealemkassahun@gmail.com>
2024-05-06 10:19:05 +0300

    feat: connect to the database

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
@@ -8,4 +11,13 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`server listening on port ${PORT}...`));
+try {
+  await mongoose.connect(process.env.MONGO_LOCAL);
+
+  app.listen(PORT, () =>
+    console.log(`DB connected && server listening on port ${PORT}...`)
+  );
+} catch (error) {
+  console.error('ERROR', error);
+  process.exit(1);
+}
