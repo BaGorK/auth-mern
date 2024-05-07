72f2026264078a320f6ab2c400a5f8c6bd1cff6b
BaGorK <edmealemkassahun@gmail.com>
2024-05-07 09:28:24 +0300

    feat: unhandled routes | server.js updated

--- server.js
+++ server.js
@@ -6,24 +6,38 @@ dotenv.config();
 import userRouter from './routes/userRoutes.js';
 import authRouter from './routes/authRoutes.js';
 import globalErrorHandlerMiddleware from './middlewares/globalErrorHandlerMiddleware.js';
+import { StatusCodes } from 'http-status-codes';
 
 const app = express();
 
+// DEVELOPMENT LOGGING MIDDELWARE
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// BODY PARSER
 app.use(express.json());
 
+// TEST ROUTE
 app.get('/api/v1/test', (req, res) => {
   return res.status(200).json({ message: 'hello server' });
 });
 
+// ROUTES
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/auth', authRouter);
 
+// UNHANDLED ROUTES
+app.use('*', (req, res) => {
+  return res
+    .status(StatusCodes.NOT_FOUND)
+    .json({ status: 'fail', message: 'Route not found' });
+});
+
+// GLOBAL ERROR HANDLER
 app.use(globalErrorHandlerMiddleware);
 
+// SERVER
 const PORT = process.env.PORT || 3000;
 
 try {
