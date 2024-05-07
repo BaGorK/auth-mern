e1b2b5729b695e75314ea71255b5063a2c373430
BaGorK <edmealemkassahun@gmail.com>
2024-05-07 19:26:58 +0300

    feat: add redux persist to our store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
 import './index.css';
-import { store } from './store/store.js';
+import { persistor, store } from './store/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 // Provider / useDispatch and / useSelector from react-redux
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
