645821eaef42474c1163d9086407f68af2d38ddc
BaGorK <edmealemkassahun@gmail.com>
2024-05-08 07:09:16 +0300

    feat: create provider | auth | get the result of signInWithPopup for google auth

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,8 +1,23 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { firebaseApp } from '../services/firebase';
+
 export default function OAuth() {
+  const handleGoogleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(firebaseApp);
+
+      const result = await signInWithPopup(auth, provider);
+      console.log(result);
+    } catch (error) {
+      console.error('could not login with google: ', error);
+    }
+  };
   return (
     <button
       type='button'
       className='bg-red-800 p-3 rounded-lg text-white uppercase hover:opacity-85 disabled:opacity-75 duration-300'
+      onClick={handleGoogleClick}
     >
       Continue with google
     </button>
--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -10,6 +10,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-
-export default firebaseApp;
+export const firebaseApp = initializeApp(firebaseConfig);
