bed67d768b758572a12e5fc953523ffadeb59c70
BaGorK <edmealemkassahun@gmail.com>
2024-05-11 08:05:27 +0300

    refactor: separate the server from the application part | create app.js and import it to server.js

--- server.js
+++ server.js
@@ -1,43 +1,8 @@
-import express from 'express';
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
-import morgan from 'morgan';
 dotenv.config();
-import userRouter from './routes/userRoutes.js';
-import authRouter from './routes/authRoutes.js';
-import globalErrorHandlerMiddleware from './middlewares/globalErrorHandlerMiddleware.js';
-import { StatusCodes } from 'http-status-codes';
-import cookieParser from 'cookie-parser';
 
-const app = express();
-
-// DEVELOPMENT LOGGING MIDDLEWARE
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
-// BODY PARSER
-app.use(express.json());
-app.use(cookieParser());
-
-// TEST ROUTE
-app.get('/api/v1/test', (req, res) => {
-  return res.status(200).json({ message: 'hello server' });
-});
-
-// ROUTES
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/auth', authRouter);
-
-// UNHANDLED ROUTES
-app.use('*', (req, res) => {
-  return res
-    .status(StatusCodes.NOT_FOUND)
-    .json({ status: 'fail', message: 'Route not found' });
-});
-
-// GLOBAL ERROR HANDLER
-app.use(globalErrorHandlerMiddleware);
+import app from './app.js';
 
 // SERVER
 const PORT = process.env.PORT || 3000;
