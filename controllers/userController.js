b67d8ebcf6f9e261205f6f85d324e3d581f1b22e
BaGorK <edmealemkassahun@gmail.com>
2024-05-11 13:18:10 +0300

    feat: complete the update user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
+import { StatusCodes } from 'http-status-codes';
+import User from '../models/userModel.js';
 import { customError } from '../utils/customError.js';
+import { hashPassword } from '../utils/passwordUtils.js';
 
 export const getAllUsers = (req, res) => {
   return res.send('get all users');
@@ -7,6 +10,34 @@ export const getAllUsers = (req, res) => {
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
     return next(customError(401, 'You can update only your account!'));
+  try {
+    if (req.body.password) {
+      req.body.password = await hashPassword(req.body.password);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          profilePicture: req.body.profilePicture,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...userWithOutPassword } = updatedUser._doc;
+    res.status(StatusCodes.OK).json({
+      status: success,
+      message: 'update successful',
+      data: {
+        user: userWithOutPassword,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 
   return res.json({ message: 'update user' });
 };
