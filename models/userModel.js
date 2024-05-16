d5c68da7035af3e60ba15228411644ce94698700
BaGorK <edmealemkassahun@gmail.com>
2024-05-16 05:45:39 +0300

    feat: add a default profile picture avatar

--- models/userModel.js
+++ models/userModel.js
@@ -17,7 +17,8 @@ const UserSchema = new mongoose.Schema(
     },
     profilePicture: {
       type: String,
-      default: '',
+      default:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.shutterstock.com%2Fimage-vector%2Fdefault-avatar-profile-icon-social-600nw-1677509740.jpg&f=1&nofb=1&ipt=12df000669e2ee261b9ea4db5b5010c27abe45a234d8d469ee3798ebb9e33249&ipo=images',
     },
   },
   { timestamps: true }
